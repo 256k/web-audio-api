a1e7bacac925d574cbc80cf84df6bfc8f893adf7
fayek helmi <fhelmi@absolunet.com>
2021-12-24 09:55:48 -0500

    added an exponential curve to kick

--- kick.js
+++ kick.js
@@ -14,12 +14,19 @@ kickButton.addEventListener('click', () => {
 
   const kickOsc = audioContext.createOscillator();
   kickOsc.frequency.setValueAtTime(140, 0);
-  kickOsc.connect(gainControl);
+  
+  // the sound is now constant. we need to create a pitch down exponential ramp
+  kickOsc.frequency.exponentialRampToValueAtTime(1, audioContext.currentTime + 0.5);
+  
+  const kickGainControl = audioContext.createGain();
+  kickGainControl.gain.setValueAtTime(1, 0);
+  kickGainControl.gain.linearRampToValueAtTime(0, audioContext.currentTime + 0.5);
+  kickOsc.connect(kickGainControl);
+  kickGainControl.connect(gainControl)
   kickOsc.start();
   
   // this will then stop 0.5s after we call the .stop() method.
-  kickOsc.stop(audioContext.currentTime + 0.5);  
-
+  kickOsc.stop(audioContext.currentTime + 0.5);    
 });
 
 
